5d73cdeeb137c2a58e03e978342b5b777d1f91ae
angeltcc <licapes@hotmail.com>
2022-08-03 22:51:09 -0300

    adicao de novos arquivos

--- login.php
+++ login.php
@@ -14,58 +14,9 @@
         <title>Bem vindo!</title>
     </head>    
     <body>
-    <header>  
-        <div class="container" id="nav-container">
-            <nav class="navbar navbar-expand-lg fixed-top">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbar-links" 
-                 aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <a class="navbar-brand ms-lg-2" href="index.php">
-                <img id="logo" src="imagens/logo1.png" alt="angelicajulia">
-                </a>
-                <a class="nav-link-style flex-shrink-0 order-lg-1 mx-auto ms-lg-0 pe-lg-2 me-lg-2" href="#">
-                <img id="logo" src="imagens/powerbutton.png" alt="sair">
-                </a>
-                <div class="collapse navbar-collapse justify-content-md-center" id="navbar-links">
-
-                      
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php">
-                                Home
-                            </a>
-                        </li>    
-                        <li class="nav-item">
-                            <a class="nav-link" href="produtos.php">
-                                Produtos
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="personalizado.php">
-                                Personalizado
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="login.php">
-                                Login
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="carrinho.php">
-                                Carrinho
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="sobre.php">
-                                Quem Somos
-                            </a>
-                        </li>
-                    </div>
-                </div>
-            </nav>
-        </div>            
-    </header>
+    <!-- header -->
+    <?php include "headerEfooter/header.inc.php"; ?>
+    <!-- body -->
         <div class="container">
         <h1> MODAL LOGIN </h1>
         </div>
